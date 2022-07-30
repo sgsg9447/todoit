4d455bfe6203703319421f23b1804712bf79a69f
sgsg9447 <sgsg9447@gmail.com>
2022-07-30 17:56:27 +0900

    feat : query이용하여 오늘날짜 보여주기

--- src/components/todolistcontainer/TodoList.tsx
+++ src/components/todolistcontainer/TodoList.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import React, { useEffect, useState } from "react";
 import { todoService } from "../../api";
 import { TodoItemType } from "../../api/types/todoItem";
@@ -7,6 +8,7 @@ type Props = {};
 
 const TodoList = (props: Props) => {
   const [items, setItems] = useState<TodoItemType[]>([]);
+  const today = dayjs().format("YYYY-MM-DD");
   const deleteTodoItem = async (id: number) => {
     const tempItems = [...items];
     todoService.deleteTodoItem(id).catch(() => {
@@ -15,10 +17,10 @@ const TodoList = (props: Props) => {
     setItems([...items].filter((item) => item.id! !== id));
   };
   useEffect(() => {
-    todoService.getTodoItems().then((data: TodoItemType[]) => {
+    todoService.getDateTodoItems(today).then((data: TodoItemType[]) => {
       setItems(data);
     });
-  }, []);
+  }, [today]);
 
   return <TodoListView items={items} handleClickDeleteBtn={deleteTodoItem} />;
 };
